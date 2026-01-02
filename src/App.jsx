c2fe373fd41fbcd7f29ff810f519cf32b389bbaf
mahade <owork.hasan@gmail.com>
2026-01-02 16:30:06 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import DaisyNav from "./components/DaisyNav/DaisyNav";
+
+function App() {
+  return (
+    <>
+      <DaisyNav></DaisyNav>
+    </>
+  );
+}
+
+export default App;
