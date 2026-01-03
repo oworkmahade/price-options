5a11c9d4b3c1b75e6d379d59ac7360898bbfcc61
mahade <owork.hasan@gmail.com>
2026-01-03 13:13:23 +0600

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
-import DaisyNav from "./components/DaisyNav/DaisyNav";
+
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
-      <DaisyNav></DaisyNav>
+      <Navbar></Navbar>
     </>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,37 @@
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import Link from "../Link/Link";
+import { useState } from "react";
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const routes = [
+    { id: 1, path: "/", name: "Home" },
+    { id: 2, path: "/about", name: "About Us" },
+    { id: 3, path: "/services", name: "Services" },
+    { id: 4, path: "/contact", name: "Contact" },
+    { id: 5, path: "/blog", name: "Blog" },
+  ];
+
   return (
     <>
-      <nav></nav>
+      <nav className=" ">
+        <div
+          onClick={() => setOpen(!open)}
+          className="md:hidden text-3xl  bg-yellow-400 p-4"
+        >
+          {open === true ? <AiOutlineClose /> : <AiOutlineMenu />}
+        </div>
+
+        <ul
+          className={`md:flex absolute md:static bg-green-400 px-8 duration-1000 p-4
+            ${open ? "top-16" : "-top-60"}
+            `}
+        >
+          {routes.map((route) => (
+            <Link key={route.id} route={route}></Link>
+          ))}
+        </ul>
+      </nav>
     </>
   );
 };
